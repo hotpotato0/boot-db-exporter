58a79829e2e450d08d07d825fc1a7a262dabd24a
윤상돈 <yunsangdon@Donys-MacBookPro.local>
2022-05-25 21:45:29 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# boot-db-exporter
---
+++ src/main/java/com/example/monitoring/controller/MetricController.java
@@ -0,0 +1,24 @@
+package com.example.monitoring.controller;
+
+
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MetricController {
+
+    @GetMapping("/test")
+    public String test() {
+        List<Tag> tags = List.of(
+                Tag.of("my_label_a", "A"),
+                Tag.of("my_label_b", "B")
+        );
+        Metrics.counter("my.demo.count.total", tags).increment();
+        return "test";
+    }
+
+}
